2b3c7eb4a17300094fbc48ebea7c19dbb9d7e229
J J <klomb@tuta.io>
2020-11-06 10:46:09 -0800

    Added IndexOf, Clear, Contains, CopyTo, Compacted, ForEach, Find

--- FixedList/FixedList.cs
+++ FixedList/FixedList.cs
@@ -6,67 +6,144 @@ namespace FixedList
     public class FixedList<T>
     {
         private readonly T[] _data;
-        private readonly bool[] _values;
+        private readonly bool[] _setValues;
+        private readonly Stack<int> _availableSlots;
         public int Count { get; private set; }
 
+        public bool IsReadOnly => true;
 
-        public readonly int Capacity;
-        private readonly Stack<int> _emptyIndices;
+        public T this[int index]
+        {
+            get => _data[index];
+            set => throw new NotSupportedException();
+        }
+
+        public int Capacity { get; private set; }
 
         public FixedList(int capacity)
         {
             Capacity = capacity;
             _data = new T[capacity];
-            _values = new bool[capacity];
-            _emptyIndices = new Stack<int>(capacity);
-            for (var i = 0; i <= capacity - 1; i++) {
-                _values[i] = false;
-                _emptyIndices.Push(i);
+            _setValues = new bool[capacity];
+            _availableSlots = new Stack<int>(capacity);
+            for (var i = 0; i < capacity - 1; i++) {
+                _setValues[i] = false;
+                _availableSlots.Push(i);
             }
         }
 
         public void Add(T value)
         {
-            var availableIndex = _emptyIndices.Pop();
+            var availableIndex = _availableSlots.Pop();
             _data[availableIndex] = value;
-            _values[availableIndex] = true;
+            _setValues[availableIndex] = true;
             Count++;
         }
 
-        private T RemoveAt(int index)
+        public void RemoveAt(int index)
         {
-            if (_values[index]) {
+            if (_setValues[index]) {
                 var removedValue = _data[index];
                 _data[index] = default;
                 Count--;
-                _emptyIndices.Push(index);
-                _values[index] = false;
-                return removedValue;
+                _availableSlots.Push(index);
+                _setValues[index] = false;
             } else {
                 throw new KeyNotFoundException(nameof(index));
             }
         }
 
-        public T Remove(T value)
+        public bool Remove(T value)
         {
-            for (var i = 0; i <= Capacity; i++) {
-                if (_values[i] && _data[i].Equals(value)) {
-                    return RemoveAt(i);
-                }
+            for (var i = 0; i < Capacity; i++) {
+                if (!_setValues[i] || !_data[i].Equals(value)) continue;
+                this.RemoveAt(i);
+                return true;
             }
-            throw new KeyNotFoundException(nameof(value));
+
+            return false;
         }
 
         public int RemoveAll(Func<T, bool> predicate)
         {
             var removed = 0;
-            for (var i = 0; i <= Capacity; i++) {
-                if (!_values[i] || !predicate(_data[i])) continue;
+            for (var i = 0; i < Capacity; i++) {
+                if (!_setValues[i] || !predicate(_data[i])) continue;
                 RemoveAt(i);
                 removed++;
             }
 
             return removed;
+        }
+
+        public int IndexOf(T item)
+        {
+            for (var i = 0; i < Capacity; i++) {
+                if (_setValues[i] && _data[i].Equals(item)) return i;
+            }
+
+            return -1;
+        }
+
+        public void Clear()
+        {
+            for (var i = 0; i < Capacity; i++) {
+                if (!_setValues[i]) continue;
+                RemoveAt(i);
+            }
+        }
+
+        public bool Contains(T item)
+        {
+            for (var i = 0; i < Capacity; i++) {
+                if (!_setValues[i] && !_data[i].Equals(item)) continue;
+                return true;
+            }
+
+            return false;
+        }
+
+        public void CopyTo(T[] array, int arrayIndex)
+        {
+            for (var i = 0; i <= Capacity; i++) {
+                if (!_setValues[i]) continue;
+                array[arrayIndex] = _data[i];
+                arrayIndex++;
+            }
+        }
+
+        public FixedList<T> Compacted(bool resize = false)
+        {
+            var newCapacity = (resize ? Count - 1 : Capacity);
+            var newFixedList = new FixedList<T>(newCapacity);
+
+            for (var i = 0; i < Capacity; i++) {
+                if (_setValues[i]) {
+                    newFixedList.Add(_data[i]);
+                }
+            }
+
+            return newFixedList;
+        }
+
+        public void ForEach(Action<T> action)
+        {
+            for (var i = 0; i < Capacity; i++) {
+                if (_setValues[i]) {
+                    action(_data[i]);
+                }
+            }
+        }
+
+        public T Find(Func<T, bool> predicate)
+        {
+            for (var i = 0; i < Capacity; i++) {
+                if (_setValues[i] && predicate(_data[i])) {
+                    return _data[i];
+                }
+            }
+
+            return default;
         }
     }
 }
